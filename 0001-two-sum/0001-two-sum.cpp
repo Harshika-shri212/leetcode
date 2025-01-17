a59e2990b7c843014c7bbd56cd8e350da4c2910a
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-01-17 11:17:23 +0530

    Time: 40 ms (35.08%), Space: 14.3 MB (54.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-
-    void solve(int start,vector<int>& curr, vector<int> &res, int t,vector<int> &nums){
-        if(t==0 && curr.size()==2){
-            res=curr;
-            return;
-        }
-
-        if(curr.size()>=2)  return;
-
-        for(int i=start;i<nums.size();i++){
-            curr.push_back(i);
-            solve(i+1,curr,res,t-nums[i],nums);
-            curr.pop_back();
-        }
-    }
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> curr;
         vector<int> res;
-        solve(0,curr,res,target,nums);
+
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = find(curr.begin(), curr.end(), nums[i]);
+            if (it != curr.end()) {
+                int foundIndex = distance(curr.begin(), it);
+                res.push_back(foundIndex);
+                res.push_back(i);
+                break;
+            } else {
+                curr.push_back(target - nums[i]);
+            }
+        }
         return res;
     }
 };
