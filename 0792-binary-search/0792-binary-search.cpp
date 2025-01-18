1f98e863f3f7e5b0bd6cad809fedea4e4ff38960
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-01-18 18:20:25 +0530

    Time: 0 ms (100%), Space: 31.3 MB (71.32%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size()-1;
-        while(left<=right){
-            int mid = (left + right)/2;
-            if(nums[mid] == target) return mid;
-            else if (nums[mid]> target )
-                right = mid-1;
-            else
-                left = mid + 1;
-        }
-        return -1;
+    int search(vector<int>& nums, int target, int low = 0, int high = INT_MIN) {
+        if (high == INT_MIN) high = nums.size() - 1;
+        if (low > high) return -1;
+
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] > target) return search(nums, target, low, mid - 1);
+        else return search(nums, target, mid + 1, high);
     }
 };
