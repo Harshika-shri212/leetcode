25ab08c97f8f1d04347f99e863f8a93b9fab7420
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-02-25 19:59:04 +0530

    Time: 0 ms (100%), Space: 28.4 MB (33.95%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,32 +1,49 @@
 class Solution {
 public:
-    TreeNode* constructFromPrePost(vector<int>& preorder,
-                                   vector<int>& postorder) {
-        int preIndex = 0;
-        int postIndex = 0;
-        return constructTree(preIndex, postIndex, preorder, postorder);
+
+    int find_index(int & num , vector<int>& postorder){
+        auto it = find(postorder.begin() , postorder.end() , num);
+        return distance(postorder.begin() , it);
     }
 
-private:
-    TreeNode* constructTree(int& preIndex, int& postIndex,
-                            vector<int>& preorder, vector<int>& postorder) {
-        TreeNode* root = new TreeNode(preorder[preIndex]);
-        preIndex++;  
-
-        
-        if (root->val != postorder[postIndex]) {
-            root->left =
-                constructTree(preIndex, postIndex, preorder, postorder);
+    int find_parent(int &num, map<int, int> &m, int &ind) {
+        int mini = INT_MAX, parent = -1;
+        for (auto &i : m) {
+            if (i.second > ind && i.second < mini) {
+                mini = i.second;
+                parent = i.first;
+            }
         }
-
-       
-        if (root->val != postorder[postIndex]) {
-            root->right =
-                constructTree(preIndex, postIndex, preorder, postorder);
-        }
-
-        postIndex++;
-
-        return root;
+        return parent;
     }
+
+
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+       map<int , int> m;
+       unordered_map<int , TreeNode*> node;
+
+       int head = preorder[0];
+       TreeNode * newNode = new TreeNode(head);
+       node[head] = newNode;
+
+       m[head] = postorder.size() - 1;
+
+       for(int i = 1 ; i < preorder.size() ; i++){
+            int num = preorder[i];
+            TreeNode * newNode = new TreeNode(num);
+
+            int ind = find_index(num , postorder);
+            m[num] = ind;
+            node[num] = newNode;
+
+            int parent = find_parent(num , m , ind);
+
+            if(!node[parent] -> left) node[parent] -> left = node[num];
+            else node[parent] -> right = node[num];
+
+       }
+       return node[head];
+    }
+
+
 };
