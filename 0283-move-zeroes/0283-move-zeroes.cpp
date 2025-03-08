cbe47af5f7739c80d64c386d0c86662e286e1204
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-03-08 19:19:40 +0530

    Time: 0 ms (100%), Space: 23.8 MB (50.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int next_non_zero_index=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[next_non_zero_index];
-                nums[next_non_zero_index]=temp;
-                next_non_zero_index+=1;
+        int z = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[z];
+                nums[z] = temp;
+                z += 1;
             }
         }
     }
