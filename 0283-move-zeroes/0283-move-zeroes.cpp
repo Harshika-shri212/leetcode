a660909e7c78cc8e3e6967735356f445d64304a6
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-03-16 15:32:00 +0530

    Time: 0 ms (100%), Space: 23.8 MB (50.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,9 +4,7 @@ public:
         int z = 0;
         for(int i = 0 ; i < nums.size() ; i++){
             if(nums[i] != 0){
-                int temp = nums[i];
-                nums[i] = nums[z];
-                nums[z] = temp;
+                swap(nums[i] , nums[z]);
                 z += 1;
             }
         }
