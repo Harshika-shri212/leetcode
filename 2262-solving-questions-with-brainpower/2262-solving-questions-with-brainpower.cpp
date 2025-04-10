02f6260c808feccad5c65e5858499a3ed344eeb9
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-04-10 10:58:20 +0530

    Time: 11 ms (27.11%), Space: 127.4 MB (37.99%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    long long mostPoints(vector<vector<int>>& questions) {
-    int n = questions.size();
-    vector<long long> dp(n + 1, 0);
-    for (int i = n - 1; i >= 0; i--) {
-        int points = questions[i][0], jump = questions[i][1];
-        dp[i] = max(points + dp[min(jump + i + 1, n)], dp[i + 1]);
+
+    long long f(vector<vector<int>>& questions , int ind , int & n , vector<long long> &dp){
+        if(ind >= n){
+            return 0;
+        }
+        if(dp[ind] != -1) return dp[ind];
+
+        int points = questions[ind][0];
+        int skip = questions[ind][1];
+
+        long long choose = points + f(questions ,ind + skip + 1 , n , dp);
+        long long skipp = f(questions ,ind + 1 ,n ,dp);
+        dp[ind] =  max(choose , skipp);
+        return dp[ind];
     }
-    return dp[0];
-}
+
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long long>dp(n,-1);
+        return f(questions , 0 , n , dp);
+    }
 };
