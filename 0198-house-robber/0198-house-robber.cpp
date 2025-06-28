de06a32f60a4a3b3521e7b14414df6375e7ab06a
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-06-28 15:16:29 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
 
-    
+    int fn(vector<int>& nums , vector<int>& dp , int ind){
+        if(ind == 0) return nums[0];
+        if(ind == 1) return nums[1];
+
+        if(dp[ind] != -1) return dp[ind];
+
+        int ans = INT_MIN;
+        for(int i = ind-2 ; i >= 0 ; i--){
+            int num = fn(nums , dp , i) + nums[ind];
+            ans = max(num , ans);
+        }
+        return dp[ind] = ans;
+    }
+
 
     int rob(vector<int>& nums) {
-        int len = nums.size();
-        vector<int>dp(len , -1);
-        
-        if(len == 0) return 0;
-        if(len == 1) return nums[0];
-
-        dp[0] = nums[0];
-        dp[1] = max(nums[0] , nums[1]);
-
-        for(int i = 2 ; i < nums.size() ; i++){
-            dp[i] = max(dp[i-1] , nums[i]  + dp[i-2]);
-        }
-
-        return dp[len-1];
+        int n = nums.size();
+        vector<int>dp(n , -1);
+        int a = fn(nums , dp , n-1);
+        int b = INT_MIN;
+        if(n-2 >= 0) b = fn(nums , dp , n-2);
+        return max(a,b);
     }
 };
