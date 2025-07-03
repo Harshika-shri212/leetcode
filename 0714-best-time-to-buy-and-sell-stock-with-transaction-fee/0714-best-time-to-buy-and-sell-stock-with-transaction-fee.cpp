4f72643dc21fc88c0f73300dfb776b1e649d21da
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-07-03 22:46:31 +0530

    Time: 100 ms (43.28%), Space: 95.1 MB (46.42%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
 
-    int fn(vector<int>& prices ,vector<vector<int>>&dp , int fees , int n , int ind , int buy){
-        if(ind == n) return 0;
-
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-
-        int pr;
-        if(buy){
-            int b1 = -prices[ind] + fn(prices , dp , fees , n , ind + 1 , 0);
-            int b2 = fn(prices , dp , fees , n , ind + 1 , 1);
-            pr = max(b1 , b2);
-        }
-        else{
-            int s1 = prices[ind] + fn(prices , dp , fees , n , ind + 1 , 1) - fees;
-            int s2 = fn(prices , dp , fees , n , ind + 1 , 0);
-            pr = max(s1 , s2);
-        }
-        return dp[ind][buy] = pr;
-    }
 
     int maxProfit(vector<int>& prices, int fees) {
         int n = prices.size();
-        vector<vector<int>>dp(n, vector<int>(2 , -1));
-        return fn(prices , dp , fees , n , 0 , 1);
+        vector<vector<int>>dp(n+1, vector<int>(2 , 0));
+        
+        for(int ind = n - 1 ; ind >= 0 ; ind--){
+            for(int buy = 0 ; buy <= 1; buy++){
+                int pr;
+                if(buy){
+                    int b1 = -prices[ind] + dp[ind + 1][0];
+                    int b2 = dp[ind + 1][1];
+                    pr = max(b1 , b2);
+                }
+                else{
+                    int s1 = prices[ind] + dp[ind + 1][1] - fees;
+                    int s2 = dp[ind + 1][0];
+                    pr = max(s1 , s2); 
+                }
+                dp[ind][buy] = pr;
+            }
+        }
+        return dp[0][1];
     }
 };
