21c3cc1dd5109659688acbae3a76564df1ce759b
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-07-03 20:42:24 +0530

    Time: 423 ms (36.31%), Space: 211.4 MB (47.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
 
-    int fn(vector<int>& prices , vector<vector<vector<int>>>& dp , int ind , int buy , int cap){
-        int n = prices.size();
-        if(cap == 0 || ind == n) return 0;
-
-        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
-
-        int pr;
-        if(buy){
-            pr =  max( (-prices[ind] + fn(prices , dp , ind + 1 , 0 , cap)) , 
-                        (fn(prices , dp , ind + 1 , 1 , cap)) );
-        }
-        else{
-            int s1 = prices[ind] + fn(prices , dp , ind + 1 , 1 , cap - 1);
-            int s2 = fn(prices , dp , ind + 1 , 0 , cap);
-            pr = max(s1 , s2);
-        }
-        return dp[ind][buy][cap] = pr;
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp (n , vector<vector<int>>(2 , vector<int>(3 , -1)));
-        return fn(prices , dp , 0 , 1 , 2);
+        vector<vector<vector<int>>>dp (n+1 , vector<vector<int>>(2 , vector<int>(3 , 0)));
+        
+        for(int i = 0 ; i <= n ; i++){
+            dp[i][0][0] = 0;
+            dp[i][1][0] = 0;
+        }
+
+        for(int buy = 0 ; buy <= 1 ; buy++){
+            for(int cap = 0 ; cap <= 2 ; cap++){
+                dp[n][buy][cap] = 0;
+            }
+        }
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int buy = 0 ; buy <= 1 ; buy++){
+                for(int cap = 1 ; cap<= 2 ; cap++){
+                    int pr;
+                    if(buy == 1){
+                        int b1 = -prices[i] + dp[i + 1][0][cap];
+                        int b2 = dp[i+1][1][cap];
+                        pr = max(b1 , b2);
+                    }
+                    else{
+                        int s1 = prices[i] + dp[i + 1][1][cap-1];
+                        int s2 = dp[i+1][0][cap];
+                        pr = max(s1 , s2);
+                    }
+                    dp[i][buy][cap] = pr;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
