123bffbc4edd7349e5897241c217e226055f78fe
Harshika-shri212 <163901064+Harshika-shri212@users.noreply.github.com>
2025-07-04 12:59:39 +0530

    Time: 31 ms (54.52%), Space: 44.4 MB (22.39%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
 
-    int fn(string& s , string& t , int n1 , int n2 , vector<vector<int>>&dp , int i , int j){
-        if(j < 0) return 1;
-        if(i < 0) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int tot;
-        if(s[i] == t[j]){
-            tot = fn(s,t,n1,n2,dp,i-1,j-1) + fn(s,t,n1,n2,dp,i-1,j);
-        }
-        else{
-            tot = fn(s,t,n1,n2,dp,i-1,j);
-        }
-        return dp[i][j] = tot;
-    }
-
     int numDistinct(string s, string t) {
         int n1 = s.size();
         int n2 = t.size();
 
-        vector<vector<int>>dp(n1 , vector<int>(n2 , -1));
+        vector<vector<double>>dp(n1 + 1 , vector<double>(n2 + 1 , 0));
 
-        return fn(s , t , n1 , n2 , dp , n1 - 1 , n2 - 1);
+        for(int i = 0 ; i <= n1 ; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int j = 1 ; j <= n2 ; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1 ; i <= n1  ; i++){
+            for(int j = 1 ; j <= n2  ; j++){
+                double tot;
+                if(s[i-1] == t[j-1]){
+                    tot = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else{
+                    tot = dp[i-1][j];
+                }
+                dp[i][j] = tot;
+            }
+        }
+        return (int)dp[n1][n2];
     }
 };
